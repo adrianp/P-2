3e075f98d7f7be754051a75c4200600b87ad1d29
adrianp <panescu.adrian.noreply@gmail.com>
2011-11-08 19:03:10 +0100

    first commit

---
+++ primes.cpp
@@ -0,0 +1,16 @@
+//to run use: mpirun -np <no of procs> /full/path/to/exe
+
+#include "mpi.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int rank, size;
+    MPI_Init( &argc, &argv );
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+    printf( "Hello world from process %d of %d\n", rank, size );
+    MPI_Finalize();
+    return 0;
+}
